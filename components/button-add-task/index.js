27f65c8c5018c64773ff131caf15bfed59b2d0a5
R31 <rodrigue@adjouale.com>
2018-11-22 10:10:34 +0100

    first commit

---
+++ components/button-add-task/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ActionButton from 'react-native-action-button';
+import { Icon } from 'react-native-elements';
+import { APP_COLORS } from '../../styles/color';
+
+const ButtonAddTask = ({ onPressCallBack }) => (
+  <ActionButton
+    buttonColor={APP_COLORS.primaryAction}
+    onPress={() => onPressCallBack()}
+  >
+    <Icon color={APP_COLORS.primaryText} name={'add'} />
+  </ActionButton>
+);
+
+
+export default ButtonAddTask;
